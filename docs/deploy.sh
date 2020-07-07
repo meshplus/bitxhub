5662ffc973bfb227263267fa63bd14765462dd00
Aiden X <caichao.xu@gmail.com>
2020-07-07 20:23:56 +0800

    docs(*): add deploy script

---
+++ docs/deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+vuepress build
+
+# navigate into the build output directory
+cd .vuepress/dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:meshplus/bitxhub.git master:gh-pages
+
