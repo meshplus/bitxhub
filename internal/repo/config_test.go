4c9c5cbe56d62034d3c283d542467c844a373d8a
jzhe <jz570889446@gmail.com>
2020-12-04 19:39:14 +0800

    refactor:revert network config

--- internal/repo/config_test.go
+++ internal/repo/config_test.go
@@ -20,15 +20,3 @@ func testReadConfig(t *testing.T) {
 		assert.True(t, uint64(i+1) == node.ID)
 	}
 }
-
-func TestReadNewConfig(t *testing.T) {
-	repo := "./testdata"
-	cfg, err := loadNetworkConfig(repo)
-	assert.Nil(t, err)
-	assert.True(t, 4 == cfg.ID)
-	assert.True(t, 4 == cfg.N)
-	assert.True(t, 4 == len(cfg.Nodes))
-	assert.True(t, "/ip4/0.0.0.0/tcp/4001" == cfg.LocalAddr)
-	assert.True(t, "/ip4/127.0.0.1/tcp/4002/p2p/QmNRgD6djYJERNpDpHqRn3mxjJ9SYiiGWzExNSy4sEmSNL" == cfg.Nodes[0].Addr)
-	assert.True(t, 3 == len(cfg.OtherNodes))
-}
