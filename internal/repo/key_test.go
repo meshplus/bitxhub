b5cba025522c9604b688062d027684690cdffefd
jzhe <jz570889446@gmail.com>
2021-02-22 15:10:54 +0800

    test:add repo and boltvm unit test

--- internal/repo/key_test.go
+++ internal/repo/key_test.go
@@ -1,1 +1,16 @@
 package repo
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestLoadKey(t *testing.T) {
+	path := "testdata/key.json"
+	_, err := LoadKey(path)
+	require.Nil(t, err)
+
+	_, err = loadPrivKey("testdata")
+	require.Nil(t, err)
+}
