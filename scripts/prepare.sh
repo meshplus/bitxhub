52168b9c311c0ba2a89752ffa8297882de0d6858
optimouskiller <shizhao-chen@qq.com>
2021-03-19 18:28:03 +0800

    fix issue #24 Add script to automatically installs the tmux based on the system type

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -24,3 +24,40 @@ fi
 if ! type mockgen >/dev/null 2>&1; then
   go get github.com/golang/mock/mockgen
 fi
+
+function Get_PM_Name()
+{
+  PM=''
+  if grep -Eqii "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+    DISTRO='CentOS'
+    PM='yum'
+  elif grep -Eqi "Red Hat Enterprise Linux Server" /etc/issue || grep -Eq "Red Hat Enterprise Linux Server" /etc/*-release; then
+    DISTRO='RHEL'
+    PM='yum'
+  elif grep -Eqi "Aliyun" /etc/issue || grep -Eq "Aliyun" /etc/*-release; then
+    DISTRO='Aliyun'
+    PM='yum'
+  elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
+    DISTRO='Fedora'
+    PM='yum'
+  elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+    DISTRO='Debian'
+    PM='apt-get'
+  elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+    DISTRO='Ubuntu'
+    PM='apt-get'
+  elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
+    DISTRO='Raspbian'
+    PM='apt-get'
+  else
+    DISTRO='unknow'
+  fi
+  print_blue "Your OS distribution is detected as: "$DISTRO;
+  eval "$1=$PM"
+}
+PM_NAME=''
+Get_PM_Name PM_NAME
+print_blue "===> 4. Install tmux with package manager"
+if [ -n "$PM_NAME" ]; then
+  $PM_NAME install -y tmux
+fi
