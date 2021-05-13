b99afd6edda79212aca0e9f4118b4866bc055d88
zhuweijie <760297795@qq.com>
2021-05-13 10:17:30 +0800

    test

--- scripts/bxh_test.sh
+++ scripts/bxh_test.sh
@@ -4,11 +4,11 @@ source x.sh
 
 CURRENT_PATH=$(pwd)
 function printHelp() {
-  print_blue "Usage:  "
-  echo "  bxh_test.sh [-b <BRANCH_NAME>] [-t <TEST_NAME>]"
-  echo "  -'b' - the branch of base ref"
-  echo "  -'t' - the test name such as bitxhub-tester,gosdk-tester,http-tester"
-  echo "  bxh_test.sh -h (print this message)"
+    print_blue "Usage:  "
+    echo "  bxh_test.sh [-b <BRANCH_NAME>] [-t <TEST_NAME>]"
+    echo "  -'b' - the branch of base ref"
+    echo "  -'t' - the test name such as bitxhub-tester,gosdk-tester,http-tester"
+    echo "  bxh_test.sh -h (print this message)"
 }
 function prepare() {
     print_blue "===> 1. Install packr"
@@ -27,14 +27,15 @@ function startBitxhub() {
     cd ../ && make cluster
 }
 function getPremo() {
-  print_blue "Start git clone Premo"
-  git clone -b "$BRANCH_NAME" https://github.com/meshplus/premo.git
+    print_blue "Start git clone Premo"
+    git clone -b "$BRANCH_NAME" https://github.com/meshplus/premo.git
 }
 function test() {
-  print_blue "Start $TEST_NAME test"
-  cd "$CURRENT_PATH"/premo && make "$TEST_NAME"
+    print_blue "Start $TEST_NAME test"
+    cd "$CURRENT_PATH"/premo && make "$TEST_NAME"
 }
 function bxh_test() {
+    prepare
     startBitxhub
 }
 while getopts "h?b:t" opt; do
