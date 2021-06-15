7152150da36be8abb0b9da1796dc86bdceb6e550
zhuweijie <760297795@qq.com>
2021-06-15 10:37:05 +0800

    update bxh_test.sh and bxh_tester.yml

--- scripts/bxh_test.sh
+++ scripts/bxh_test.sh
@@ -10,6 +10,40 @@ function printHelp() {
     echo "  -'t' - the test name such as bitxhub-tester,gosdk-tester,http-tester"
     echo "  bxh_test.sh -h (print this message)"
 }
+
+function Get_PM_Name(){
+  PM=''
+  if [ "$(uname)" == "Darwin" ]; then
+    DISTRO='MacOS'
+    PM='brew'
+  elif grep -Eqii "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+    DISTRO='CentOS'
+    PM='yum'
+  elif grep -Eqi "Red Hat Enterprise Linux Server" /etc/issue || grep -Eq "Red Hat Enterprise Linux Server" /etc/*-release; then
+    DISTRO='RHEL'
+    PM='yum'
+  elif grep -Eqi "Aliyun" /etc/issue || grep -Eq "Aliyun" /etc/*-release; then
+    DISTRO='Aliyun'
+    PM='yum'
+  elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
+    DISTRO='Fedora'
+    PM='yum'
+  elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+    DISTRO='Debian'
+    PM='apt-get'
+  elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+    DISTRO='Ubuntu'
+    PM='apt-get'
+  elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
+    DISTRO='Raspbian'
+    PM='apt-get'
+  else
+    DISTRO='unknow'
+  fi
+  print_blue "Your OS distribution is detected as: "$DISTRO;
+  eval "$1=$PM"
+}
+
 function prepare() {
     print_blue "===> 1. Install packr"
     if ! type packr >/dev/null 2>&1; then
@@ -17,11 +51,19 @@ function prepare() {
     fi
     print_blue "===> 2. Install tmux with package manager"
     if ! type tmux >/dev/null 2>&1; then
-      sudo apt-get install -y tmux
+      PM_NAME=''
+      Get_PM_Name PM_NAME
+      if [ -n "$PM_NAME" ]; then
+        if [ "$PM_NAME" == "brew" ]; then
+          $PM_NAME install tmux
+        else
+          sudo "$PM_NAME" install -y tmux
+        fi
+      fi
     fi
 }
 
-function startBitxhub() {
+function start_bxh_solo() {
     print_blue "Start bitxhub"
     echo "$CURRENT_PATH"
     cd ../ && make install && cd scripts
@@ -29,6 +71,7 @@ function startBitxhub() {
     nohup bash solo.sh 2>gc.log 1>solo.log &
     sleep 100
 }
+
 function bitxhub_tester() {
     print_blue "Start git clone Premo"
     echo "$BRANCH_NAME"
@@ -37,11 +80,13 @@ function bitxhub_tester() {
     print_blue "Start test"
     make bitxhub-tester REPORT=Y
 }
+
 function bxh_test() {
     prepare
     startBitxhub
     bitxhub_tester
 }
+
 while getopts "h?b:" opt; do
   case "$opt" in
   h | \?)
