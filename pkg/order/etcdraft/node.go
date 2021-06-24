a7dbc5168c7741f794267d9dd019246ae57b76fb
zhourong <zhourong@hyperchain.cn>
2021-06-24 19:50:40 +0800

    fix(raft): set seqNo if leader is just elected
    (cherry picked from commit 04d6e07bd08369b11b4f74a0ca861d2f895e53e4)

--- pkg/order/etcdraft/node.go
+++ pkg/order/etcdraft/node.go
@@ -396,6 +396,7 @@ func (n *Node) run() {
 			}
 
 			if n.justElected {
+				n.mempool.SetBatchSeqNo(n.lastExec)
 				msgInflight := n.ramLastIndex() > n.appliedIndex+1
 				if msgInflight {
 					n.logger.Debug("There are in flight blocks, new leader should not generate new batches")
