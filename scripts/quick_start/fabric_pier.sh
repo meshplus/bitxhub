8a42661330ade29736de71ea7e22a7aa40f98856
Ruoxin <liruoxin@hyperchain.cn>
2021-07-07 22:34:47 +0800

    fix:remove go mod

--- scripts/quick_start/fabric_pier.sh
+++ scripts/quick_start/fabric_pier.sh
@@ -111,6 +111,7 @@ function start() {
   prepare
 
   pier --repo="${PIER_ROOT}" init
+  mkdir -p "${PIER_ROOT}"/plugins
   cp "${CURRENT_PATH}"/pier-client-fabric/build/fabric-client-1.4.so "${PIER_ROOT}"/plugins/
   cp -rf "${CURRENT_PATH}"/pier-client-fabric/config "${PIER_ROOT}"/fabric
   rm -rf "${PIER_ROOT}"/fabric/crypto-config
