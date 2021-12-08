33f8239cd71c1473d1a6dda06be9f8f5724a521a
Ruoxin <liruoxin@hyperchain.cn>
2021-12-08 15:41:22 +0800

    fix(cmd): remove key convert

--- cmd/bitxhub/key.go
+++ cmd/bitxhub/key.go
@@ -10,7 +10,6 @@ import (
 	"github.com/meshplus/bitxhub-kit/crypto/asym"
 	"github.com/meshplus/bitxhub-kit/fileutil"
 	"github.com/meshplus/bitxhub/internal/repo"
-	libp2pcert "github.com/meshplus/go-libp2p-cert"
 	"github.com/urfave/cli"
 )
 
@@ -34,27 +33,6 @@ func keyCMD() cli.Command {
 					},
 				},
 				Action: genPrivKey,
-			},
-			{
-				Name:  "convert",
-				Usage: "Convert the Secp256k1 private key to BitXHub key format",
-				Flags: []cli.Flag{
-					cli.BoolFlag{
-						Name:  "save,s",
-						Usage: "Save BitXHub key into repo",
-					},
-					cli.StringFlag{
-						Name:     "priv",
-						Usage:    "Specify private key path",
-						Required: true,
-					},
-					cli.StringFlag{
-						Name:     "passwd",
-						Usage:    "Specify password",
-						Required: false,
-					},
-				},
-				Action: convertKey,
 			},
 			{
 				Name:   "show",
@@ -81,7 +59,6 @@ func keyCMD() cli.Command {
 		},
 	}
 }
-
 
 func genPrivKey(ctx *cli.Context) error {
 	target := ctx.String("target")
@@ -115,51 +92,6 @@ func genPrivKey(ctx *cli.Context) error {
 	return nil
 }
 
-func convertKey(ctx *cli.Context) error {
-	privPath := ctx.String("priv")
-	passwd := ctx.String("passwd")
-
-	if passwd == "" {
-		passwd = repo.DefaultPasswd
-	}
-
-	data, err := ioutil.ReadFile(privPath)
-	if err != nil {
-		return fmt.Errorf("read private key: %w", err)
-	}
-
-	privKey, err := libp2pcert.ParsePrivateKey(data, crypto.Secp256k1)
-	if err != nil {
-		return err
-	}
-
-	if ctx.Bool("save") {
-		repoRoot, err := repo.PathRootWithDefault(ctx.GlobalString("repo"))
-		if err != nil {
-			return err
-		}
-
-		keyPath := filepath.Join(repoRoot, repo.KeyName)
-		if err := asym.StorePrivateKey(privKey, keyPath, passwd); err != nil {
-			return err
-		}
-	} else {
-		keyStore, err := asym.GenKeyStore(privKey, passwd)
-		if err != nil {
-			return err
-		}
-
-		pretty, err := keyStore.Pretty()
-		if err != nil {
-			return err
-		}
-
-		fmt.Println(pretty)
-	}
-
-	return nil
-}
-
 func showKey(ctx *cli.Context) error {
 	repoRoot, err := repo.PathRootWithDefault(ctx.GlobalString("repo"))
 	if err != nil {
@@ -188,7 +120,6 @@ func getAddress(ctx *cli.Context) error {
 	if err != nil {
 		return err
 	}
-
 
 	addr, err := privKey.PublicKey().Address()
 	if err != nil {
