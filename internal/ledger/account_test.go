943c6b840832ef2f02cb0ad2c237bd3fe503d2dd
liweixia <790890362@qq.com>
2022-01-11 14:15:03 +0800

    test: add ledger uni test

--- internal/ledger/account_test.go
+++ internal/ledger/account_test.go
@@ -2,6 +2,7 @@ package ledger
 
 import (
 	"io/ioutil"
+	"math/big"
 	"path/filepath"
 	"testing"
 
@@ -35,6 +36,9 @@ func TestAccount_GetState(t *testing.T) {
 	stateLedger := ledger.StateLedger.(*SimpleLedger)
 	account := newAccount(stateLedger.ldb, stateLedger.accountCache, addr, newChanger())
 
+	addr1 := account.GetAddress()
+	assert.Equal(t, addr, addr1)
+
 	account.SetState([]byte("a"), []byte("b"))
 	ok, v := account.GetState([]byte("a"))
 	assert.True(t, ok)
@@ -48,8 +52,35 @@ func TestAccount_GetState(t *testing.T) {
 	ok, v = account.GetState([]byte("a"))
 	assert.False(t, ok)
 	assert.Nil(t, v)
+	account.GetCommittedState([]byte("a"))
 }
 
 func TestAccount_AddState(t *testing.T) {
+
+}
+
+func TestAccount_AccountBalance(t *testing.T) {
+	repoRoot, err := ioutil.TempDir("", "ledger_commit")
+	assert.Nil(t, err)
+	blockStorage, err := leveldb.New(filepath.Join(repoRoot, "storage"))
+	assert.Nil(t, err)
+	ldb, err := leveldb.New(filepath.Join(repoRoot, "ledger"))
+	assert.Nil(t, err)
+
+	accountCache, err := NewAccountCache()
+	assert.Nil(t, err)
+	logger := log.NewWithModule("account_test")
+	blockFile, err := blockfile.NewBlockFile(repoRoot, logger)
+	assert.Nil(t, err)
+	ledger, err := New(createMockRepo(t), blockStorage, ldb, blockFile, accountCache, log.NewWithModule("ChainLedger"))
+	assert.Nil(t, err)
+
+	h := hexutil.Encode(bytesutil.LeftPadBytes([]byte{11}, 20))
+	addr := types.NewAddressByStr(h)
+	stateLedger := ledger.StateLedger.(*SimpleLedger)
+	account := newAccount(stateLedger.ldb, stateLedger.accountCache, addr, newChanger())
+
+	account.AddBalance(big.NewInt(1))
+	account.SubBalance(big.NewInt(1))
 
 }
