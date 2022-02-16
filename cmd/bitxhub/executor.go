ef6203e9dd5d17777b5a74de9e6b29723668cd71
dawn-to-dusk <1927529251@qq.com>
2022-02-16 09:36:26 +0800

    test(*): for executor test

--- cmd/bitxhub/executor.go
+++ cmd/bitxhub/executor.go
@@ -319,7 +319,7 @@ func genTransferTransaction(repoRoot string, addresses []*types.Address, logger 
 
 	td := &pb.TransactionData{
 		Type:   pb.TransactionData_NORMAL,
-		Amount: "1000000000000000",
+		Amount: "10000000000000000000000000000000",
 	}
 
 	payload, _ := td.Marshal()
