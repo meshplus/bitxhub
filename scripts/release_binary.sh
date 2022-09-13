4563f34d766ded7cc3aae053cd10fbe96fce1e86
jiuhuche120 <760297795@qq.com>
2022-09-13 18:07:46 +0800

    fix(*) fix get packr version
    (cherry picked from commit 215204f76b7d3c7630d8bdf9bbfb9dc92bef6ecb)

--- scripts/release_binary.sh
+++ scripts/release_binary.sh
@@ -12,7 +12,7 @@ rm -rf "$BUILD_PATH"
 mkdir -p "$BUILD_PATH"
 print_blue "===> 1. Install packr"
 if ! type packr >/dev/null 2>&1; then
-  go get -u github.com/gobuffalo/packr/packr
+  go_install github.com/gobuffalo/packr/v2/packr2@v2.8.3
 fi
 
 print_blue "===> 2. Build bitxhub"
