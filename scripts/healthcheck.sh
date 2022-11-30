1ffa7cf640f0bba6666482a1cca53a7234790cbf
jiuhuche120 <760297795@qq.com>
2022-11-30 11:28:57 +0800

    fix dockerfile for zombie process situation

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env bash
-status=$(bitxhub client --gateway http://localhost:${GateWayPort}/v1 chain status)
-if [ $status = "normal" ]; then
-    exit 0;
+#!/usr/bin/env sh
+gateway_port=$(grep -m 1 gateway </root/.bitxhub/bitxhub.toml | awk '{print $3}')
+status=$(bitxhub client --gateway http://localhost:"$gateway_port"/v1 chain status)
+if [ "$status" = "normal" ]; then
+  exit 0
 else
-    exit 1;
+  exit 1
 fi
