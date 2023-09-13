11193e08ac059e6653afc770cae4daaf4aeba4c7
yzh-d <18730047077@163.com>
2023-09-13 18:34:08 +0800

    resolve conflicts

--- internal/executor/system/governance/node_manager_test.go
+++ internal/executor/system/governance/node_manager_test.go
@@ -182,6 +182,92 @@ func TestNodeManager_RunForPropose(t *testing.T) {
 	}
 }
 
+func TestNodeManager_RunForNodeUpgradePropose(t *testing.T) {
+	nm := NewNodeManager(&common.SystemContractConfig{
+		Logger: logrus.New(),
+	})
+
+	mockCtl := gomock.NewController(t)
+	stateLedger := mock_ledger.NewMockStateLedger(mockCtl)
+
+	accountCache, err := ledger.NewAccountCache()
+	assert.Nil(t, err)
+	repoRoot := t.TempDir()
+	ld, err := leveldb.New(filepath.Join(repoRoot, "node_manager"))
+	assert.Nil(t, err)
+	account := ledger.NewAccount(ld, accountCache, types.NewAddressByStr(common.NodeManagerContractAddr), ledger.NewChanger())
+
+	stateLedger.EXPECT().GetOrCreateAccount(gomock.Any()).Return(account).AnyTimes()
+	stateLedger.EXPECT().SetBalance(gomock.Any(), gomock.Any()).AnyTimes()
+	stateLedger.EXPECT().AddLog(gomock.Any()).AnyTimes()
+
+	err = InitCouncilMembers(stateLedger, []*repo.Admin{
+		{
+			Address: admin1,
+			Weight:  1,
+			Name:    "111",
+		},
+		{
+			Address: admin2,
+			Weight:  1,
+			Name:    "222",
+		},
+		{
+			Address: admin3,
+			Weight:  1,
+			Name:    "333",
+		},
+		{
+			Address: admin4,
+			Weight:  1,
+			Name:    "444",
+		},
+	}, "10")
+	assert.Nil(t, err)
+	err = InitNodeMembers(stateLedger, []*NodeMember{
+		{
+			NodeId: "16Uiu2HAmJ38LwfY6pfgDWNvk3ypjcpEMSePNTE6Ma2NCLqjbZJSF",
+		},
+	})
+
+	testcases := []struct {
+		Caller   string
+		Data     []byte
+		Expected vm.ExecutionResult
+		Err      error
+	}{
+		{
+			Caller: admin1,
+			Data: generateNodeUpgradeProposeData(t, NodeExtraArgs{
+				Nodes: []*NodeMember{
+					{
+						NodeId: "16Uiu2HAmJ38LwfY6pfgDWNvk3ypjcpEMSePNTE6Ma2NCLqjbZJSF",
+					},
+				},
+			}),
+			Expected: vm.ExecutionResult{
+				UsedGas: NodeManagementProposalGas,
+			},
+			Err: nil,
+		},
+	}
+
+	for _, test := range testcases {
+		nm.Reset(stateLedger)
+
+		res, err := nm.Run(&vm.Message{
+			From: types.NewAddressByStr(test.Caller).ETHAddress(),
+			Data: test.Data,
+		})
+
+		assert.Equal(t, test.Err, err)
+		if res != nil {
+			assert.Equal(t, uint64(NodeManagementProposalGas), res.UsedGas)
+			assert.Equal(t, test.Expected.Err, res.Err)
+		}
+	}
+}
+
 func TestNodeManager_GetNodeMembers(t *testing.T) {
 
 	mockCtl := gomock.NewController(t)
@@ -232,7 +318,7 @@ func TestNodeManager_GetNodeMembers(t *testing.T) {
 	fmt.Println("GetNodeMembers-members:", members)
 }
 
-func TestNodeManager_RunForVote(t *testing.T) {
+func TestNodeManager_RunForAddVote(t *testing.T) {
 	nm := NewNodeManager(&common.SystemContractConfig{
 		Logger: logrus.New(),
 	})
@@ -467,6 +553,119 @@ func TestNodeManager_RunForRemoveVote(t *testing.T) {
 	}
 }
 
+func TestNodeManager_RunForUpgradeVote(t *testing.T) {
+	nm := NewNodeManager(&common.SystemContractConfig{
+		Logger: logrus.New(),
+	})
+
+	mockCtl := gomock.NewController(t)
+	stateLedger := mock_ledger.NewMockStateLedger(mockCtl)
+
+	accountCache, err := ledger.NewAccountCache()
+	assert.Nil(t, err)
+	repoRoot := t.TempDir()
+	ld, err := leveldb.New(filepath.Join(repoRoot, "node_manager"))
+	assert.Nil(t, err)
+	account := ledger.NewAccount(ld, accountCache, types.NewAddressByStr(common.NodeManagerContractAddr), ledger.NewChanger())
+
+	stateLedger.EXPECT().GetOrCreateAccount(gomock.Any()).Return(account).AnyTimes()
+	stateLedger.EXPECT().SetBalance(gomock.Any(), gomock.Any()).AnyTimes()
+	stateLedger.EXPECT().AddLog(gomock.Any()).AnyTimes()
+
+	err = InitCouncilMembers(stateLedger, []*repo.Admin{
+		{
+			Address: admin1,
+			Weight:  1,
+			Name:    "111",
+		},
+		{
+			Address: admin2,
+			Weight:  1,
+			Name:    "222",
+		},
+		{
+			Address: admin3,
+			Weight:  1,
+			Name:    "333",
+		},
+		{
+			Address: admin4,
+			Weight:  1,
+			Name:    "444",
+		},
+	}, "10")
+	assert.Nil(t, err)
+	err = InitNodeMembers(stateLedger, []*NodeMember{
+		{
+			NodeId: "16Uiu2HAmJ38LwfY6pfgDWNvk3ypjcpEMSePNTE6Ma2NCLqjbZJSF",
+		},
+	})
+
+	// propose
+	nm.Reset(stateLedger)
+	_, err = nm.Run(&vm.Message{
+		From: types.NewAddressByStr(admin1).ETHAddress(),
+		Data: generateNodeUpgradeProposeData(t, NodeExtraArgs{
+			Nodes: []*NodeMember{
+				{
+					NodeId: "16Uiu2HAmJ38LwfY6pfgDWNvk3ypjcpEMSePNTE6Ma2NCLqjbZJSF",
+				},
+			},
+		}),
+	})
+
+	assert.Nil(t, err)
+
+	testcases := []struct {
+		Caller   string
+		Data     []byte
+		Expected vm.ExecutionResult
+		Err      error
+	}{
+		{
+			Caller: admin1,
+			Data:   generateNodeVoteData(t, nm.proposalID.GetID()-1, Pass),
+			Expected: vm.ExecutionResult{
+				UsedGas: NodeManagementVoteGas,
+			},
+			Err: nil,
+		},
+		{
+			Caller: admin1,
+			Data:   generateNodeVoteData(t, nm.proposalID.GetID()-1, Pass),
+			Expected: vm.ExecutionResult{
+				UsedGas: NodeManagementVoteGas,
+				Err:     ErrUseHasVoted,
+			},
+			Err: nil,
+		},
+		{
+			Caller: "0x1000000000000000000000000000000000000000",
+			Data:   generateNodeVoteData(t, nm.proposalID.GetID()-1, Pass),
+			Expected: vm.ExecutionResult{
+				UsedGas: NodeManagementVoteGas,
+				Err:     ErrNotFoundCouncilMember,
+			},
+			Err: nil,
+		},
+	}
+
+	for _, test := range testcases {
+		nm.Reset(stateLedger)
+
+		result, err := runVoteMethod(nm, &vm.Message{
+			From: types.NewAddressByStr(test.Caller).ETHAddress(),
+			Data: test.Data,
+		})
+
+		assert.Equal(t, test.Err, err)
+
+		if result != nil {
+			assert.Equal(t, test.Expected.UsedGas, result.UsedGas)
+		}
+	}
+}
+
 func TestNodeManager_EstimateGas(t *testing.T) {
 	nm := NewNodeManager(&common.SystemContractConfig{
 		Logger: logrus.New(),
@@ -538,6 +737,20 @@ func generateNodeRemoveProposeData(t *testing.T, extraArgs NodeExtraArgs) []byte
 	extra, err := json.Marshal(extraArgs)
 	assert.Nil(t, err)
 	data, err := gabi.Pack(ProposeMethod, uint8(NodeRemove), title, desc, blockNumber, extra)
+	assert.Nil(t, err)
+	return data
+}
+
+func generateNodeUpgradeProposeData(t *testing.T, extraArgs NodeExtraArgs) []byte {
+	gabi, err := GetABI()
+	assert.Nil(t, err)
+
+	title := "title"
+	desc := "desc"
+	blockNumber := uint64(1000)
+	extra, err := json.Marshal(extraArgs)
+	assert.Nil(t, err)
+	data, err := gabi.Pack(ProposeMethod, uint8(NodeUpgrade), title, desc, blockNumber, extra)
 	assert.Nil(t, err)
 	return data
 }
